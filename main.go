e7678f76168f69164d6dc34df5f87a05262398aa
Adler <nkj20932@hotmail.com>
2018-05-29 22:26:44 +1000

    print file content in loop

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/adlerhsieh/interviewer/question"
 	"gopkg.in/yaml.v2"
@@ -27,9 +28,31 @@ func readQuestions() []question.Question {
 }
 
 var questions = readQuestions()
+var hasError bool
 
 func main() {
+	for i := 0; i < len(questions); i++ {
+		q := &questions[i]
+
+		data, err := ioutil.ReadFile("./question/files/" + q.Filename)
+		if err != nil {
+			if strings.Contains(err.Error(), "no such file or directory") {
+				fmt.Println("File not found:\n  question: " + q.Question + "\n  file location: ./question/files/" + q.Filename)
+				hasError = true
+			} else {
+				panic(err)
+			}
+		}
+
+		q.Content = string(data)
+	}
+
+	if hasError {
+		os.Exit(1)
+	}
+
 	for _, question := range questions {
+		fmt.Println(question.Content)
 		fmt.Println(question.Question)
 		fmt.Print("> ")
 		scanner := bufio.NewScanner(os.Stdin)
--- question/question.go
+++ question/question.go
@@ -3,5 +3,6 @@ package question
 type Question struct {
 	Question string `yaml:question`
 	Answer   string `yaml:answer`
-	Content  string `yaml:content`
+	Filename string `yaml:filename`
+	Content  string
 }
